fc7519f2b3c4be553633ee4bfcac431bc160047c
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-12 19:13:22 -0300

    replication instance

--- aws-dms/main.tf
+++ aws-dms/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+locals {
+  project_name         = "dms-migration"
+  availability_zone_1a = "sa-east-1a"
+  availability_zone_1b = "sa-east-1b"
+}
+
 ### Migration Bucket ###
 
 # resource "aws_s3_bucket" "main" {
@@ -37,6 +43,111 @@ provider "aws" {
 # data "aws_iam_policy" "amazon_s3_fullaccess" {
 #   arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 # }
+
+### VPC ###
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  # Enable DNS hostnames 
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-${local.project_name}"
+  }
+}
+
+### Internet Gateway ###
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-${local.project_name}"
+  }
+}
+
+### Route Tables ###
+
+resource "aws_default_route_table" "internet" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "internet-rt"
+  }
+}
+
+### Subnets ###
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = local.availability_zone_1a
+
+  # Auto-assign public IPv4 address
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.project_name}-public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = local.availability_zone_1b
+
+  # Auto-assign public IPv4 address
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.project_name}-public2"
+  }
+}
+
+
+### Security Group ###
+
+# Clean-up Default
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group" "main" {
+  name        = "${local.project_name}-public-sc"
+  description = "Allow Traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.project_name}-public-sc"
+  }
+}
+
+resource "aws_security_group_rule" "all_inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+### DMS ###
 
 # dms-vpc-role
 
@@ -74,13 +185,21 @@ resource "aws_iam_role_policy_attachment" "dms_access_for_endpoint_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSRedshiftS3Role"
 }
 
+resource "aws_dms_replication_subnet_group" "main" {
+  replication_subnet_group_description = "DMS replication subnet"
+  replication_subnet_group_id          = "dms-replication-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.public1.id, aws_subnet.public2.id
+  ]
+}
 
 # Create a new replication instance
 resource "aws_dms_replication_instance" "test" {
   allocated_storage          = 20
   apply_immediately          = true
   auto_minor_version_upgrade = true
-  availability_zone          = "sa-east-1a"
+  availability_zone          = local.availability_zone_1a
   # engine_version             = "3.4.7"
   # kms_key_arn                  = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
   multi_az = false
@@ -88,15 +207,15 @@ resource "aws_dms_replication_instance" "test" {
   publicly_accessible         = true
   replication_instance_class  = "dms.t2.micro"
   replication_instance_id     = "dms-replication-instance"
-  # replication_subnet_group_id = aws_dms_replication_subnet_group.test-dms-replication-subnet-group-tf.id
+  replication_subnet_group_id = aws_dms_replication_subnet_group.main.id
 
   tags = {
     Name = "dms-replication-instance"
   }
 
-  # vpc_security_group_ids = [
-  #   "sg-12345678",
-  # ]
+  vpc_security_group_ids = [
+    aws_security_group.main.id
+  ]
 
   depends_on = [
     aws_iam_role_policy_attachment.dms_vpc_role_attach,
