2b3c63fd3450ebb2180965a6b3b7d7cb0c9f8a92
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-13 10:02:47 -0300

    output

--- ec2-mysql/main.tf
+++ ec2-mysql/main.tf
@@ -170,3 +170,11 @@ resource "aws_instance" "main" {
   }
 
 }
+
+output "instance_ip" {
+  value = aws_instance.main.public_ip
+}
+
+output "instance_dns" {
+  value = aws_instance.main.public_dns
+}
